026dbe568bfebe0ab8e1698ed9a653bad08f6fef
mperry37 <mperry37@alaska.edu>
2020-04-01 14:31:27 -0800

    feat: adds circle

--- shape.hpp
+++ shape.hpp
@@ -14,5 +14,15 @@ public:
     virtual void generatePostScript(std::ostream& os) const = 0;
 };
 
+class Circle : public Shape {
+public:
+    Circle(double radius);
+    double getHeight() const override;
+    double getWidth() const override;
+    void generatePostScript(std::ostream& os) const override;
+private:
+    double _radius;
+};
+
 
 #endif
